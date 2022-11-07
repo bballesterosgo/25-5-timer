7ab79aa1129b2bbdfeef46fc499e7b023c968d08
Bernardo <gurmock@gmail.com>
2022-11-07 06:47:56 +0100

    creando componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  
+      <div className='title-container'>
+        <h1 className='title'>25 + 5 Clock</h1>
+      </div>
+      <div className='clock-container'>
+      
+      </div>
     </div>
   );
 }
