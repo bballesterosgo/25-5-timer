5a1975a5d49273cd55c50e8a83106cbdaaeb7542
Bernardo <gurmock@gmail.com>
2022-11-08 07:10:58 +0100

    break-label

--- src/components/BreakLabel.jsx
+++ src/components/BreakLabel.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { GoDash,GoPlus } from "react-icons/go";
 
 import '../stylesheet/BreakLabel.css'
 
 function BreakLabel ({ breakLength }){
 
   return <div className='break-label' id='break-label'>
-    <p>{breakLength}</p>
-  </div>
+             <div className='break-label-title'>
+            <h2>Break Length</h2>
+          </div>
+          <div className='break-label-controls'>
+            <GoDash />
+            <p>{ breakLength }</p>
+            <GoPlus />
+          </div>
+        </div>
 }
 
 export default BreakLabel;
