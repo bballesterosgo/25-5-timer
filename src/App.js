a6e3dfc1f8848119597121cdad5799b7403d047d
Bernardo <gurmock@gmail.com>
2022-11-08 07:27:19 +0100

    me voy a trabajar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{ useState } from 'react';
 import './App.css';
 import BreakLabel from './components/BreakLabel';
+import ClockDisplay from './components/ClockDisplay';
 import SessionLabel from './components/SessionLabel';
 
 function App() {
@@ -15,10 +16,15 @@ const [breakLength,setBreakLength] = useState(5);
         <h1 className='title'>25 + 5 Clock</h1>
       </div>
       <div className='clock-container'>
-      <BreakLabel 
-        breakLength={breakLength}/>
-      <SessionLabel 
-        sessionLength={sessionLength}/>
+        <div className='break-session-controls-container'>
+          <BreakLabel 
+            breakLength={breakLength}/>
+          <SessionLabel 
+            sessionLength={sessionLength}/>
+        </div>
+      <div className='display-container'>
+        <ClockDisplay />
+      </div>
       </div>
     </div>
   );
---
+++ src/components/ClockDisplay.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ClockDisplay () {
+  return <div>
+    <p>Session</p>
+    25
+  </div>
+}
+
+export default ClockDisplay;
