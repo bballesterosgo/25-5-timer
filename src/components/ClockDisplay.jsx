cb1d7dd5024897de6098d8ed65f51c3bc3f78ba3
Bernardo <gurmock@gmail.com>
2022-11-12 11:54:02 +0100

    startStop

--- src/components/ClockDisplay.jsx
+++ src/components/ClockDisplay.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { AiOutlinePlayCircle,AiOutlinePauseCircle,AiOutlineSync } from "react-icons/ai";
+import { FaRegStopCircle } from "react-icons/fa";
+
 import moment from "moment/moment";
 import momentDurationFormatSetup from "moment-duration-format";
 
@@ -8,9 +10,11 @@ import '../stylesheet/ClockDisplay.css'
 //MM:SS 25:00
 momentDurationFormatSetup(moment);
 
-
-
 function ClockDisplay ({sessionLength,breakLength}) {
+
+const [currentSessionType, setCurrentSessionType] = useState('Session');//Session or Break
+
+const [intervalId,setIntervalId] = useState(null);  
 
 const [timeleft, setTimeLeft] = useState(sessionLength);
 
@@ -20,21 +24,56 @@ useEffect(() =>{
 },[sessionLength])
 
 //Start Stop click
+const isStarted =  intervalId !== null;
+
 const handleStartStopClick = () => {
-  //Decrement time left bt 1 every second(1000ms)
-  setInterval()
+  if(isStarted){
+     //If we are in started mod we want to top the timer clearInterval
+    clearInterval(intervalId);
+    setIntervalId(null);
+
+
+  }else{
+      //Decrement time left bt 1 every second(1000ms)
+    const newIntervalId = setInterval(() =>{
+      setTimeLeft(prevTimeLeft => {
+       const newTimeLeft = prevTimeLeft -1;
+       if(newTimeLeft >= 0){
+         return prevTimeLeft -1;
+       }
+       if(currentSessionType === 'Session'){
+        //switch to break setTimeLeft to breakSessionLength
+        setCurrentSessionType('Break');
+        setTimeLeft(breakLength);
+       }else if(currentSessionType === 'Break'){
+        //Switch to session and set time left to sessionLength
+        setCurrentSessionType('Session');
+        setTimeLeft(sessionLength)
+       }
+      
+
+
+      })
+     },100);//TODO: change to 1000
+     setIntervalId(newIntervalId)
+  }
+
 };
 
 //Time format MM:SS 25:00
-const formattedTimeLeft = moment.duration(timeleft,'s').format('mm:ss')
+const formattedTimeLeft = moment.duration(timeleft,'s').format('mm:ss',{trim: false})
 
   return <div>
-    <p>Session</p>
-    <p>{formattedTimeLeft}</p>
-    <AiOutlinePlayCircle onClick={handleStartStopClick}/>
-    <AiOutlinePauseCircle />
-    <AiOutlineSync />
-  </div>
+            <p id='timer-label'>{currentSessionType}</p>
+            <p id='time-left'>{formattedTimeLeft}</p>
+          <div className="start-stop-pause-buttons">
+            <div onClick={handleStartStopClick}>
+              {isStarted ? <FaRegStopCircle /> : <AiOutlinePlayCircle/>}
+            </div>
+              <AiOutlinePauseCircle />
+              <AiOutlineSync />
+             </div>
+          </div>
 }
 
 export default ClockDisplay;
