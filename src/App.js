003a44ca7495fc26eb6ea1a0680c2cacd3f8b203
Bernardo <gurmock@gmail.com>
2022-11-12 12:26:01 +0100

    app props and hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import BreakLabel from "./components/BreakLabel";
 import ClockDisplay from "./components/ClockDisplay";
 import SessionLabel from "./components/SessionLabel";
@@ -6,9 +6,17 @@ import SessionLabel from "./components/SessionLabel";
 import "./App.css";
 
 function App() {
-
- //Session state and functions 
+  //Session state and functions
   const [sessionLength, setSessionLength] = useState(1500);
+  const [currentSessionType, setCurrentSessionType] = useState("Session"); //Session or Break
+
+  const [intervalId, setIntervalId] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(sessionLength);
+
+  //change timeLeft when sessionLength changes
+  useEffect(() => {
+    setTimeLeft(sessionLength);
+  }, [sessionLength]);
 
   const decrementSessionLength = () => {
     const newSessionLength = sessionLength - 60;
@@ -41,6 +49,47 @@ function App() {
     setBreakLength(breakLength + 60);
   };
 
+  //Start Stop click
+  const isStarted = intervalId !== null;
+
+  const handleStartStopClick = () => {
+    if (isStarted) {
+      //If we are in started mod we want to top the timer clearInterval
+      clearInterval(intervalId);
+      setIntervalId(null);
+    } else {
+      //Decrement time left bt 1 every second(1000ms)
+      const newIntervalId = setInterval(() => {
+        setTimeLeft((prevTimeLeft) => {
+          const newTimeLeft = prevTimeLeft - 1;
+          if (newTimeLeft >= 0) {
+            return prevTimeLeft - 1;
+          }
+          if (currentSessionType === "Session") {
+            //switch to break setTimeLeft to breakSessionLength
+            setCurrentSessionType("Break");
+            setTimeLeft(breakLength);
+          } else if (currentSessionType === "Break") {
+            //Switch to session and set time left to sessionLength
+            setCurrentSessionType("Session");
+            setTimeLeft(sessionLength);
+          }
+        });
+      }, 100); //TODO: change to 1000
+      setIntervalId(newIntervalId);
+    }
+  };
+
+  //Reset Button
+  const handleResetButtonClick = () => {
+    //Clear time out interval
+    //Set the interval to null
+    //Set sessionType to 'Session'
+    //Set sessionLength to 25 min
+    //Reset break length to 5 min
+    //Reset timer to 25 min
+  };
+
   return (
     <div className="App">
       <div className="title-logo-container">
@@ -62,7 +111,11 @@ function App() {
         <div className="display-container">
           <ClockDisplay
             breakLength={breakLength}
-            sessionLength={sessionLength} />
+            sessionLength={sessionLength}
+            handleStartStopClick={handleStartStopClick}
+            startStopButtonLabel={isStarted}
+            timeLeft={timeLeft}
+            timeLabel={currentSessionType} />
         </div>
       </div>
     </div>
--- src/components/ClockDisplay.jsx
+++ src/components/ClockDisplay.jsx
@@ -10,65 +10,17 @@ import '../stylesheet/ClockDisplay.css'
 //MM:SS 25:00
 momentDurationFormatSetup(moment);
 
-function ClockDisplay ({sessionLength,breakLength}) {
-
-const [currentSessionType, setCurrentSessionType] = useState('Session');//Session or Break
-
-const [intervalId,setIntervalId] = useState(null);  
-
-const [timeleft, setTimeLeft] = useState(sessionLength);
-
-//change timeLeft when sessionLength changes
-useEffect(() =>{
-  setTimeLeft(sessionLength)
-},[sessionLength])
-
-//Start Stop click
-const isStarted =  intervalId !== null;
-
-const handleStartStopClick = () => {
-  if(isStarted){
-     //If we are in started mod we want to top the timer clearInterval
-    clearInterval(intervalId);
-    setIntervalId(null);
-
-
-  }else{
-      //Decrement time left bt 1 every second(1000ms)
-    const newIntervalId = setInterval(() =>{
-      setTimeLeft(prevTimeLeft => {
-       const newTimeLeft = prevTimeLeft -1;
-       if(newTimeLeft >= 0){
-         return prevTimeLeft -1;
-       }
-       if(currentSessionType === 'Session'){
-        //switch to break setTimeLeft to breakSessionLength
-        setCurrentSessionType('Break');
-        setTimeLeft(breakLength);
-       }else if(currentSessionType === 'Break'){
-        //Switch to session and set time left to sessionLength
-        setCurrentSessionType('Session');
-        setTimeLeft(sessionLength)
-       }
-      
-
-
-      })
-     },100);//TODO: change to 1000
-     setIntervalId(newIntervalId)
-  }
-
-};
+function ClockDisplay ({handleStartStopClick,timeLabel,startStopButtonLabel,timeLeft}) {
 
 //Time format MM:SS 25:00
-const formattedTimeLeft = moment.duration(timeleft,'s').format('mm:ss',{trim: false})
+const formattedTimeLeft = moment.duration(timeLeft,'s').format('mm:ss',{trim: false})
 
   return <div>
-            <p id='timer-label'>{currentSessionType}</p>
+            <p id='timer-label'>{timeLabel}</p>
             <p id='time-left'>{formattedTimeLeft}</p>
           <div className="start-stop-pause-buttons">
             <div onClick={handleStartStopClick}>
-              {isStarted ? <FaRegStopCircle /> : <AiOutlinePlayCircle/>}
+              { startStopButtonLabel ? <FaRegStopCircle /> : <AiOutlinePlayCircle/>}
             </div>
               <AiOutlinePauseCircle />
               <AiOutlineSync />
